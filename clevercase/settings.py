486934c32769883fe45fea171a38d8d4cfa1f344
Lukovskiy Max <88405806+lukovskiy54@users.noreply.github.com>
2023-12-07 16:41:18 +0200

    Update settings.py

--- clevercase/settings.py
+++ clevercase/settings.py
@@ -81,12 +81,8 @@ WSGI_APPLICATION = 'clevercase.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'dbclevercase', 
-        'USER': 'postgres',
-        'PASSWORD': '1489',
-        'HOST': '127.0.0.1', 
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
